383e6d4b2c4c4837805c900c7ca27f56b24254b5
Evgen <ackold27@gmail.co>
2024-03-02 09:00:12 +0200

    start

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<div>ID {data.product.id}</div>
+<div>TITLE {data.product.idtitle}</div>
+<img src={data.product.thumbnail} alt="thumbnailImage" />
